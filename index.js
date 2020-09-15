3cd5d437aac394ebbf01c43031e6b789aa947581
Andy D <mindovermiles262@gmail>
2020-09-14 22:14:31 -0500

    static json response

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+const members = [
+  { "id": 1, "name": "John", "email": "john@example.com", "status": "active" },
+  { "id": 2, "name": "Bob", "email": "bob@example.com", "status": "inactive" },
+  { "id": 3, "name": "Sue", "email": "sue@example.com", "status": "active" }
+];
 
 // Set a static folder
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// Gets all members
+app.get('/api/members', (req, resp) => {
+  resp.json(members); 
+})
  
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
