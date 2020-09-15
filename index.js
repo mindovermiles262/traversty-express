6fb7328b59888b1821a0de177ed61521cae77334
Andy D <mindovermiles262@gmail>
2020-09-14 22:51:15 -0500

    added eslint

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const moment = require('moment');
 
+const app = express();
+
 const PORT = process.env.PORT || 5000;
-const members = require('./members.js')
+const members = require('./members.js');
 
 // Define middleware
 const logger = (req, resp, next) => {
-  console.log(`${moment().format()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
-  next()
-}
+  console.log(`${moment().format()}: ${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  next();
+};
 
 // Init logger middleware
 app.use(logger);
-
 
 // Set a static folder
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Gets all members
 app.get('/api/members', (req, resp) => {
-  resp.json(members); 
-})
- 
+  resp.json(members);
+});
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
