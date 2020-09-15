6c583fa70f27600c7f9353256e36af2e89155086
Andy D <mindovermiles262@gmail>
2020-09-14 21:53:36 -0500

    manual routing and serving of static page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+
 app.get('/', (req, resp) => {
-  resp.send('Hello Express!'); 
+  resp.sendFile(path.join(__dirname, 'public', 'index.html'));
 })
   
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
